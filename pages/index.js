436fadb09864d9f089e7ff778318e022799f5766
Kingi Gilbert <kingi@ignitestudios.org>
2021-06-26 00:55:10 +1200

    push changes

--- pages/index.js
+++ pages/index.js
@@ -28,8 +28,7 @@ const SignupLink = () => (
       <div>
         <h3>Sign up for an account</h3>
         <p>
-          Sign up and sign in to explore all the features provided by Clerk
-          out-of-the-box
+          Sign up and sign in to explore all the resources to help you on your journey.
         </p>
       </div>
       <div className={styles.arrow}>
@@ -47,8 +46,8 @@ const SignupLink = () => (
 // https://docs.clerk.dev/frontend/react/signedin-and-signedout
 const Main = () => (
   <main className={styles.main}>
-    <h1 className={styles.title}>Welcome to your new app</h1>
-    <p className={styles.description}>Sign up for an account to get started</p>
+    <h1 className={styles.title}>Nau mai ki everydayMāori</h1>
+    <p className={styles.description}>Kia rere</p>
 
     <div className={styles.cards}>
       <div className={styles.card}>
@@ -60,53 +59,15 @@ const Main = () => (
         </SignedOut>
       </div>
 
-      <div className={styles.card}>
-        <Link href="https://dashboard.clerk.dev">
-          <a target="_blank" rel="noreferrer" className={styles.cardContent}>
-            <img src="/icons/settings.svg" />
-            <div>
-              <h3>Configure settings for your app</h3>
-              <p>
-                Visit Clerk to manage instances and configure settings for user
-                management, theme, and more
-              </p>
-            </div>
-            <div className={styles.arrow}>
-              <img src="/icons/arrow-right.svg" />
-            </div>
-          </a>
-        </Link>
-      </div>
     </div>
 
-    <div className={styles.links}>
-      <Link href="https://docs.clerk.dev">
-        <a target="_blank" rel="noreferrer" className={styles.link}>
-          <span className={styles.linkText}>Read Clerk documentation</span>
-        </a>
-      </Link>
-      <Link href="https://nextjs.org/docs">
-        <a target="_blank" rel="noreferrer" className={styles.link}>
-          <span className={styles.linkText}>Read NextJS documentation</span>
-        </a>
-      </Link>
-    </div>
   </main>
 );
 
 // Footer component
 const Footer = () => (
   <footer className={styles.footer}>
-    <a
-      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Powered by{" "}
-      <img src="/clerk.svg" alt="Clerk.dev" className={styles.logo} />
-      +
-      <img src="/nextjs.svg" alt="Next.js" className={styles.logo} />
-    </a>
+    ❤️
   </footer>
 );
 
